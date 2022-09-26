d6211f04810629c3a486b41f98b52257a9dd5ac8
bbbozhena <bbbozhenka@gmail.com>
2022-09-26 18:36:51 +0300

    add comp

---
+++ src/components/app/App.jsx
@@ -0,0 +1,19 @@
+
+import React from "react"
+//Components
+import TodoItem from "../todoitems/todoitems.jsx"
+
+
+export default function App() {
+  return (
+    <div>
+<h1 className="text-3xl font-bold text-center">
+      Tasks
+    </h1>
+    <TodoItem />
+    </div>
+    
+  )
+}
+
+
---
+++ src/components/todoitems/todoitems.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./todoitem.css";
+
+const TodoItem = () => {
+    return (
+        <div>
+        <div className="item-container text-center">
+            Go to shop
+        </div>
+        </div>
+
+    )
+}
+export default TodoItem;
